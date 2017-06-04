3d8fbee4864dca60fc214c27a51da8edf0285eb8
Pierre-Marie <lageneau@telecom-paristech.fr>
2017-06-04 20:42:33 +0200

    Ajout d'images + Wow such responsive

--- js/pyramid.js
+++ js/pyramid.js
@@ -1,6 +1,6 @@
 // Constants:
-var rowsModelSize = [5,30,42];
-var rowsViewSize = [1,3,5];
+var rowsModelSize = [1,5,11,21,39];
+var rowsViewSize = [1,3,5,7,9];
 
 /*  The pyramid contains three thing:
     Paths to the pictures it might display: modelRows. There are more than actually displayed
@@ -43,6 +43,7 @@ function loadData(){
             console.log(pyramid);
         }
     }
+	console.log(pyramid.modelRows)
 }
 
 // Gather the DOM elements representing the pyramid and fill them with pictures
@@ -50,28 +51,71 @@ function firstPyramidSetup(){
   /*  Lets fill the viewRows section of the pyramid with the DOM elements that are the pictures of it
       Plus we change the pictures on the fly so that we don't always have the same one everywhere
   */
-  for(r=0; r<pyramid.modelRows.length; r++){
-    var tempRow = [];
+	var tree = document.getElementById("pyramid");
+	for(r=0; r<pyramid.modelRows.length; r++){
+		
+		//Creation ligne
+		var HTMLrow = document.createElement("ul");
+		tree.appendChild(HTMLrow);
+		HTMLrow.setAttribute("style","margin-left: auto;margin-right: auto;text-align: center;")
+		HTMLrow.setAttribute("id","PyramidRow"+r)
+		
+		
+		
+		//Creation button gauche
+		if(rowsViewSize[r]!==1){
+			var li = document.createElement("li");
+			var button = document.createElement("button");
+			button.type = "button";
+			button.setAttribute("class","buttonpyramid");
+			button.setAttribute("onclick","rotateRow("+r+",1);")
+			li.appendChild(button);
+			HTMLrow.appendChild(li);
+		}
+		
+		console.log("r = "+r)
+		var tempRow = [];
+		
+	//Remplissage de la ligne
     for(im=0; im<rowsViewSize[r]; im++){
-      var cell = document.getElementById("r" + r + "im" + im);
-      cell.src = pyramid.modelRows[r][im].source;
-      tempRow.push(cell);
+		console.log("img = "+im)
+		var HTMLli = document.createElement("li");
+		var cell = document.createElement("img");
+		cell.id = "r" + r + "im" + im;
+		cell.src = pyramid.modelRows[r][im].source;
+		cell.setAttribute("class", "pyramidPicture");
+		cell.setAttribute("style","width:"+100/(pyramid.modelRows.length*2)+"%;")
+		console.log(cell)
+		HTMLli.appendChild(cell);
+		HTMLrow.appendChild(HTMLli);
+		tempRow.push(cell);
     }
+	
     pyramid.viewRows.push(tempRow);
+	
+	//Creation du bouton droit
+	if(rowsViewSize[r]!==1){
+		var li = document.createElement("li");
+		var button = document.createElement("button");
+		button.setAttribute("onclick","rotateRow("+r+",-1);")
+		button.type = "button";
+		button.setAttribute("class","buttonpyramid");
+		li.appendChild(button);
+		HTMLrow.appendChild(li);
+	}
   }
 }
 
 
 /*  This function changes the pictures displayed on the page by rotating them by one following the modeRows
     Row: wich row of the pyramid is supposed to be changed
-    Direction: +1 for left and -1 for right. (A bit counter intuitive I know)
+    Direction: +1 for left and -1 for right. (A bit counter intuitive I know) 
 */
 function rotateRow(row, direction){
-
   // What were we displaying before and where are we going
   var startIdx = pyramid.idxModel[row] + direction;
   var modelRowLength = pyramid.modelRows[row].length;
-
+  
   // Solves problem of negative index
   if(startIdx == -1){
     startIdx = modelRowLength - 1;
