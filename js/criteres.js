9d3c9ec1544a4ba76080e17db5a4c83ffedb6462
cmolle <chloe.molle@telecom-paristech.fr>
2017-06-16 11:48:48 +0200

    wip slider

--- js/criteres.js
+++ js/criteres.js
@@ -1,6 +1,5 @@
 function getSucre(){
   var s = document.getElementById("sucre");
-  console.log(s.value);
   return(s.value);
 }
 
@@ -23,38 +22,41 @@ function getCalories(){
 var draggedElement = null;
 document.addEventListener('dragstart',function(e){
   draggedElement = e.target;
+  var parent = draggedElement.parentNode;
+  if(parent.className != "dropper5"){
+    parent.getElementsByClassName("red")[0].style.visibility = "hidden";
+    e.dataTransfer.setData('Number',parent.getElementsByClassName("slider")[0].value);
+  }
+
+});
+
+document.addEventListener('dragend',function(e){
+  draggedElement = e.target;
+  var parent = draggedElement.parentNode;
+  if(parent.className != "dropper5"){
+    //parent.getElementsByClassName("red")[0].style.visibility = "visible";
+  }
 });
 
 
 var elements = document.getElementsByClassName("dropper");
-var isDropped = false;
 for(var i = 0; i < elements.length; i ++){
   var dropper = elements[i];
   dropper.addEventListener('drop',function(e){
-    if(!isDropped){
       e.preventDefault();
-      document.getElementById("premier").style.visibility = "visible";
-      e.target.append(draggedElement);
-    }
-    else{
-      return(true);
-    }
+      var parent = draggedElement.parentNode;
+      dropper.getElementsByClassName("red")[0].style.visibility = "visible";
+      if(parent.className != "dropper5"){
+        e.target.getElementsByClassName("slider")[0].value = e.dataTransfer.getData('Number');
+        parent.getElementsByClassName("slider")[0].id = draggedElement.getElementsByClassName("id")[0].innerHTML
+      }
+      e.target.prepend(draggedElement);
   });
   dropper.addEventListener('dragenter', function(e) {
-    if(! isDropped){
       e.preventDefault();
-    }
-    else{
-      return(true);
-    }
   });
   dropper.addEventListener('dragover', function(e) {
-    if(! isDropped){
       e.preventDefault();
-    }
-    else{
-      return(true);
-    }
   });
 }
 
@@ -62,98 +64,70 @@ for(var i = 0; i < elements.length; i ++){
 
 
 var elements = document.getElementsByClassName("dropper2");
-var isDropped2 = false;
 for(var i = 0; i < elements.length; i ++){
   var dropper2 = elements[i];
   dropper2.addEventListener('drop',function(e){
-    if(! isDropped2){
-      e.preventDefault();
-      e.target.append(draggedElement);
+    e.preventDefault();
+    var parent = draggedElement.parentNode;
+    dropper.getElementsByClassName("red")[0].style.visibility = "visible";
+    if(parent.className != "dropper5"){
+      e.target.getElementsByClassName("slider")[0].value = e.dataTransfer.getData('Number');
+      parent.getElementsByClassName("slider")[0].id = draggedElement.getElementsByClassName("id")[0].innerHTML
     }
-    else{
-      return(true);
-    }
+    e.target.prepend(draggedElement);
+
   });
   dropper2.addEventListener('dragenter', function(e) {
-    if(! isDropped2){
       e.preventDefault();
-    }
-    else{
-      return(true);
-    }
   });
   dropper2.addEventListener('dragover', function(e) {
-    if(! isDropped2){
       e.preventDefault();
-    }
-    else{
-      return(true);
-    }
   });
 }
 
 
 var elements = document.getElementsByClassName("dropper3");
-var isDropped3 = false;
 for(var i = 0; i < elements.length; i ++){
   var dropper3 = elements[i];
   dropper3.addEventListener('drop',function(e){
-    if(! isDropped3){
-      e.preventDefault();
-      e.target.append(draggedElement);
+    e.preventDefault();
+    var parent = draggedElement.parentNode;
+    dropper.getElementsByClassName("red")[0].style.visibility = "visible";
+    if(parent.className != "dropper5"){
+      e.target.getElementsByClassName("slider")[0].value = e.dataTransfer.getData('Number');
+      parent.getElementsByClassName("slider")[0].id = draggedElement.getElementsByClassName("id")[0].innerHTML
     }
-    else{
-      return(true);
-    }
+    e.target.prepend(draggedElement);
+
   });
   dropper3.addEventListener('dragenter', function(e) {
-    if(! isDropped3){
       e.preventDefault();
-    }
-    else{
-      return(true);
-    }
   });
   dropper3.addEventListener('dragover', function(e) {
-    if(! isDropped3){
       e.preventDefault();
-    }
-    else{
-      return(true);
-    }
-  });
+});
 }
 
 
 var elements = document.getElementsByClassName("dropper4");
-var isDropped4 = false;
 for(var i = 0; i < elements.length; i ++){
   var dropper4 = elements[i];
   dropper4.addEventListener('drop',function(e){
-    if(! isDropped4){
-      e.preventDefault();
-      e.target.append(draggedElement);
+    e.preventDefault();
+    var parent = draggedElement.parentNode;
+    dropper.getElementsByClassName("red")[0].style.visibility = "visible";
+    if(parent.className != "dropper5"){
+      e.target.getElementsByClassName("slider")[0].value = e.dataTransfer.getData('Number');
+      parent.getElementsByClassName("slider")[0].id = draggedElement.getElementsByClassName("id")[0].innerHTML
     }
-    else{
-      alert(target.getElementsByClassName("draggable"));
-      return(true);
-    }
+    e.target.prepend(draggedElement);
+
   });
   dropper4.addEventListener('dragenter', function(e) {
-    if(! isDropped4){
-      e.preventDefault();
-    }
-    else{
-      return(true);
-    }
+    e.preventDefault();
   });
   dropper4.addEventListener('dragover', function(e) {
-    if(! isDropped4){
       e.preventDefault();
-    }
-    else{
-      return(true);
-    }
   });
 }
 
@@ -161,16 +135,13 @@ var elements = document.getElementsByClassName("dropper5");
 for(var i = 0; i < elements.length; i ++){
   var dropper5 = elements[i];
   dropper5.addEventListener('drop',function(e){
-      console.log("drop");
       e.preventDefault();
-      e.target.append(draggedElement);
+      e.target.prepend(draggedElement);
   });
   dropper5.addEventListener('dragenter', function(e) {
-      console.log("enter");
       e.preventDefault();
   });
   dropper5.addEventListener('dragover', function(e) {
-      console.log("over");
       e.preventDefault();
   });
 }
