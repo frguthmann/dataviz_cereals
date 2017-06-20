c67643a3d7bfd36f12f57d219ed06afd18346ca0
François Guthmann <francois.guthmann@telecom-paristech.fr>
2017-06-20 16:42:27 +0200

    Refactored code a bit

--- js/criteres.js
+++ js/criteres.js
@@ -34,110 +34,29 @@ document.addEventListener('dragend',function(e){
   }
 });
 
+for(var idx=0; idx<droppers.length; idx++){
+    var elements = document.getElementsByClassName(droppers[idx]);
+    for(var i = 0; i < elements.length; i ++){
+        var dropper = elements[i];
+        dropper.addEventListener('drop',function(e){
+          e.preventDefault();
+          var parent = draggedElement.parentNode;
+          if(parent.className != "dropper5" && droppers[idx]!="dropper5"){
+            e.target.getElementsByClassName("slider")[0].value = e.dataTransfer.getData('Number');
+            parent.getElementsByClassName("slider")[0].id = draggedElement.getElementsByClassName("id")[0].innerHTML
+          }
+          e.target.prepend(draggedElement);
+        });
 
-var elements = document.getElementsByClassName("dropper");
-for(var i = 0; i < elements.length; i ++){
-  var dropper = elements[i];
-  dropper.addEventListener('drop',function(e){
-      e.preventDefault();
-      var parent = draggedElement.parentNode;
-      if(parent.className != "dropper5"){
-        e.target.getElementsByClassName("slider")[0].value = e.dataTransfer.getData('Number');
-        parent.getElementsByClassName("slider")[0].id = draggedElement.getElementsByClassName("id")[0].innerHTML
-      }
-      e.target.prepend(draggedElement);
-  });
-  dropper.addEventListener('dragenter', function(e) {
-      e.preventDefault();
-  });
-  dropper.addEventListener('dragover', function(e) {
-      e.preventDefault();
-  });
-}
+        dropper.addEventListener('dragenter', function(e) {
+          e.preventDefault();
+        });
 
-
-
-
-var elements = document.getElementsByClassName("dropper2");
-for(var i = 0; i < elements.length; i ++){
-  var dropper2 = elements[i];
-  dropper2.addEventListener('drop',function(e){
-    e.preventDefault();
-    var parent = draggedElement.parentNode;
-    if(parent.className != "dropper5"){
-      e.target.getElementsByClassName("slider")[0].value = e.dataTransfer.getData('Number');
-      parent.getElementsByClassName("slider")[0].id = draggedElement.getElementsByClassName("id")[0].innerHTML
+        dropper.addEventListener('dragover', function(e) {
+          e.preventDefault();
+        });
     }
-    e.target.prepend(draggedElement);
-
-  });
-  dropper2.addEventListener('dragenter', function(e) {
-      e.preventDefault();
-  });
-  dropper2.addEventListener('dragover', function(e) {
-      e.preventDefault();
-  });
 }
-
-
-var elements = document.getElementsByClassName("dropper3");
-for(var i = 0; i < elements.length; i ++){
-  var dropper3 = elements[i];
-  dropper3.addEventListener('drop',function(e){
-    e.preventDefault();
-    var parent = draggedElement.parentNode;
-    if(parent.className != "dropper5"){
-      e.target.getElementsByClassName("slider")[0].value = e.dataTransfer.getData('Number');
-      parent.getElementsByClassName("slider")[0].id = draggedElement.getElementsByClassName("id")[0].innerHTML
-    }
-    e.target.prepend(draggedElement);
-
-  });
-  dropper3.addEventListener('dragenter', function(e) {
-      e.preventDefault();
-  });
-  dropper3.addEventListener('dragover', function(e) {
-      e.preventDefault();
-});
-}
-
-
-var elements = document.getElementsByClassName("dropper4");
-for(var i = 0; i < elements.length; i ++){
-  var dropper4 = elements[i];
-  dropper4.addEventListener('drop',function(e){
-    e.preventDefault();
-    var parent = draggedElement.parentNode;
-    if(parent.className != "dropper5"){
-      e.target.getElementsByClassName("slider")[0].value = e.dataTransfer.getData('Number');
-      parent.getElementsByClassName("slider")[0].id = draggedElement.getElementsByClassName("id")[0].innerHTML
-    }
-    e.target.prepend(draggedElement);
-
-  });
-  dropper4.addEventListener('dragenter', function(e) {
-    e.preventDefault();
-  });
-  dropper4.addEventListener('dragover', function(e) {
-      e.preventDefault();
-  });
-}
-
-var elements = document.getElementsByClassName("dropper5");
-for(var i = 0; i < elements.length; i ++){
-  var dropper5 = elements[i];
-  dropper5.addEventListener('drop',function(e){
-      e.preventDefault();
-      e.target.prepend(draggedElement);
-  });
-  dropper5.addEventListener('dragenter', function(e) {
-      e.preventDefault();
-  });
-  dropper5.addEventListener('dragover', function(e) {
-      e.preventDefault();
-  });
-}
-
 
 
 var drag = document.getElementsByClassName("draggable");
